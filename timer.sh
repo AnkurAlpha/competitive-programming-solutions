8c0e5ef94f4ec5aecbd5ffa12fd54d914bfd0613
Ankur Kumar <ankuralpha404@gmail.com>
2024-11-04 20:58:38 +0530

    03 domino piling

---
+++ timer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this is just a timer script that I run while doing any code problem
+
+sleep 900 # only 15 minutes per question
+while true
+do
+    sleep 2
+    notify-send "time up !!"
+done
