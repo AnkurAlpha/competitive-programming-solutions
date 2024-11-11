e9a377964a6d480779215cc7604de8e5c7d8723e
Ankur Kumar <ankuralpha404@gmail.com>
2024-11-11 20:28:12 +0530

    08 helpful math

---
+++ codeforces/07 petya and strings.cpp
@@ -0,0 +1,23 @@
+#include <cwctype>
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+int main() {
+    // cout << int('a'); // for getting to know ascii values or a character
+    string a , b;
+    cin >> a;
+    cin >> b ;
+    int i = 0 ;
+    for ( int x = 0 ; x < a.length() ; ++x) {
+            char l = tolower(a.at(x));
+            char m = towlower(b.at(x));
+            if ( int(l) >  int(m) ) {
+                ++i ;
+            }
+            if  ( int(l) < int(m) ) {
+                --i ;
+            }
+    }
+    cout << i ;
+}
---
+++ codeforces/08 helpful math.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+    string a;
+    cin >> a;
+    vector<int> vec;
+    for (int i = 0; i < a.length(); i = i + 2) {
+        char x = a.at(i);
+        string y = to_string(x);
+        int z = stoi(y);
+        vec.push_back(z);
+    }
+    vector<int>::iterator i1, i2;
+    for (i1 = vec.begin(); i1 != vec.end(); i1++) {
+        for (i2 = i1; i2 != vec.end(); i2++) {
+            if (*i1 > *i2) {
+                int x = *i1;
+                *i1 = *i2;
+                *i2 = x;
+            }
+        }
+    }
+    for (i1 = vec.begin(); i1 != vec.end(); ++i1) {
+        cout << *i1-48;
+        if (i1 != vec.end() - 1) {
+            cout << "+";
+        }
+    }
+}
