71f48c3fe5e6c866eac6bbefd51c63e0eb6fc48d
Ankur Kumar <ankuralpha404@gmail.com>
2024-11-15 00:40:06 +0530

    bor or girl

---
+++ codeforces/10 boy or girl.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    string a;
+    cin >> a;
+    vector<char> vec;
+    for (char i : a) {
+        int x = count(vec.begin(), vec.end(), i);
+        if ( x == 0 ) {
+            vec.push_back(i);
+        }
+    }
+    int b = vec.size() ;
+    if (b%2 == 0 ) {
+        cout << "CHAT WITH HER!";
+    } else {
+        cout << "IGNORE HIM!" ;
+    }
+}
