28ac4e5d44b0361903b6c79f496d190ab56a871d
Ankur Kumar <ankuralpha404@gmail.com>
2024-11-15 00:45:19 +0530

    bear and big brohter

---
+++ codeforces/11 bear and big brother.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int i = 0 ;
+    while (a <= b ) {
+        ++i ;
+        a = a * 3 ;
+        b = b * 2 ;
+    }
+    cout << i ;
+}
